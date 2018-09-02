1d6dea832016ebf1593eb6c038db343613852884
Lucier Ferreira Lima <lucierflima@gmail.com>
2018-09-02 03:52:21 -0300

    correcao configuracao

--- sinsermu/settings.py
+++ sinsermu/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'g&u))%0(nbgs8$^*bem*woszz5jnbn88pw!137$#1@%&chzu5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
